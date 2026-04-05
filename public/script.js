7da860830216b1a34ae8590ec36b04f2c10320c6
modernmaximuz <purgatorydaen@gmail.com>
2026-04-05 14:44:17 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,18 +1,5 @@
-const content = document.getElementById("content");
-
-async function load() {
-    const res = await fetch("/load");
-    const data = await res.json();
-    content.value = data.paste.content;
-}
-
-async function save() {
-    await fetch("/save", {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ content: content.value })
-    });
-    alert("Saved!");
-}
-
-load();
+await fetch("/save", {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ content: content.value })
+});
