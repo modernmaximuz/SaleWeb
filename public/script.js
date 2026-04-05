f55c7fdafb1aa54587874b56713f4bbf1da1cbd0
modernmaximuz <purgatorydaen@gmail.com>
2026-04-05 17:37:07 +0800

    Added firebase

--- public/script.js
+++ public/script.js
@@ -1,3 +1,7 @@
+import { db } from './firebase.js';
+import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+
+
 const content = document.getElementById("content");
 const saveBtn = document.getElementById("saveBtn");
 
