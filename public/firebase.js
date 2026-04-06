54b21bc56f659fa3ea3a24747629c09eb2e446be
modernmaximuz <purgatorydaen@gmail.com>
2026-04-06 11:18:33 +0800

    Create firebase.js

---
+++ public/firebase.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJhF0Hkz1kIfjb75BKzcHXfHfdZQd-5fM",
+  authDomain: "saleweb-ad2f4.firebaseapp.com",
+  projectId: "saleweb-ad2f4"
+};
+
+firebase.initializeApp(firebaseConfig);
