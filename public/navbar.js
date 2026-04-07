0e2376feb4a19d33568e487de1002a6cdff8f038
modernmaximuz <purgatorydaen@gmail.com>
2026-04-07 11:01:06 +0800

    Update navbar.js

--- public/navbar.js
+++ public/navbar.js
@@ -2,9 +2,9 @@ document.querySelectorAll(".navBtn").forEach(btn => {
     btn.addEventListener("click", () => {
         const text = btn.textContent.trim();
 
-        if (text === "Restocks") window.location = "/restocks.html";
-        if (text === "Proofs") window.location = "/proofs.html";
-        if (text === "Support") window.location = "/support.html";
+        if (text === "Restocks") window.location = "/restocks";
+        if (text === "Proofs") window.location = "/proofs";
+        if (text === "Support") window.location = "/support";
     });
 });
 
