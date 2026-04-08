0cc9791c9d1ce0110d69dcea53ccc733c376bd7f
modernmaximuz <purgatorydaen@gmail.com>
2026-04-08 13:20:39 +0800

    Create orderBot.js

---
+++ orderBot.js
@@ -0,0 +1,38 @@
+const { Client, GatewayIntentBits, PermissionsBitField } = require("discord.js");
+
+const client = new Client({
+    intents:[GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
+});
+
+client.on("ready", ()=>console.log("Order bot ready"));
+
+async function createOrderChannel(order) {
+    const guild = await client.guilds.fetch(process.env.GUILD_ID);
+    const member = await guild.members.fetch(order.discordId);
+
+    const channel = await guild.channels.create({
+        name: `${order.user}-customer`,
+        type: 0,
+        parent: "1491299595018305566",
+        permissionOverwrites: [
+            {
+                id: guild.id,
+                deny: [PermissionsBitField.Flags.ViewChannel]
+            },
+            {
+                id: member.id,
+                allow: [
+                    PermissionsBitField.Flags.ViewChannel,
+                    PermissionsBitField.Flags.SendMessages,
+                    PermissionsBitField.Flags.AttachFiles
+                ]
+            }
+        ]
+    });
+
+    channel.send("Order:\n" + JSON.stringify(order.items, null, 2));
+}
+
+client.login(process.env.ORDER_BOT_TOKEN);
+
+module.exports = { createOrderChannel };
