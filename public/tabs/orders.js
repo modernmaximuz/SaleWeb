4cb87ab6a314f14bcaf410666f6790e24e5b9bc5
modernmaximuz <purgatorydaen@gmail.com>
2026-04-08 13:16:51 +0800

    Update orders.js

--- public/tabs/orders.js
+++ public/tabs/orders.js
@@ -1,1 +1,47 @@
+const ORDER_PASTE = "NEW_PASTE_ID_FOR_ORDERS";
 
+function renderCart() {
+    const cart = getCart();
+    const box = document.getElementById("cartView");
+    box.innerHTML = "";
+
+    cart.forEach(i => {
+        box.innerHTML += `
+        <div class="cartItem">
+            <img src="${i.img}">
+            ${i.name} x${i.qty}
+            <button onclick="changeQty('${i.name}',1)">+</button>
+            <button onclick="changeQty('${i.name}',-1)">-</button>
+            <button onclick="removeFromCart('${i.name}')">Remove</button>
+        </div>`;
+    });
+}
+renderCart();
+
+document.getElementById("placeOrder").onclick = async () => {
+    const user = await (await fetch("/me")).json();
+    const cart = getCart();
+
+    const order = {
+        user: user.username,
+        discordId: user.id,
+        date: new Date().toISOString(),
+        items: cart,
+        status: "pending"
+    };
+
+    const res = await fetch(`/load/${ORDER_PASTE}`);
+    const json = await res.json();
+    const data = JSON.parse(json.content || "[]");
+
+    data.push(order);
+
+    await fetch(`/save/${ORDER_PASTE}`, {
+        method:"PUT",
+        headers:{ "Content-Type":"application/json" },
+        body: JSON.stringify({ content: JSON.stringify(data,null,2) })
+    });
+
+    localStorage.removeItem("hades_cart");
+    alert("Order placed!");
+};
